f5625c75fc938276b9eea29931403c493443a20b
Dave Nolan <dpn.nolan@gmail.com>
2019-12-20 10:22:00 -0500

    Updates

---
+++ precache-manifest.4605628a3ecbca61fecfbd6bb1854540.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd617d9d6034281ea7800dccb7b91800",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "2a17aa3f4d5c4bbeb7fd",
+    "url": "/memory-game/static/css/main.c7e44b26.chunk.css"
+  },
+  {
+    "revision": "d301ce4dad67b86faeb5",
+    "url": "/memory-game/static/js/2.25c38a69.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory-game/static/js/2.25c38a69.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a17aa3f4d5c4bbeb7fd",
+    "url": "/memory-game/static/js/main.a0093198.chunk.js"
+  },
+  {
+    "revision": "27bb3e093d76e9005e99",
+    "url": "/memory-game/static/js/runtime-main.457aaf77.js"
+  }
+]);
